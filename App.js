b599d2c0b95b18a62d8f083fba740ace2ccb8362
Sanaboina Swetha <126000859+SanaboinaSwetha@users.noreply.github.com>
2023-07-30 18:54:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import BmiCalculator from './BmiCalculator/BmiCalculator.js';
+function App() {
+  return (
+    <div className="App">
+      <BmiCalculator />
+    </div>
+  );
+}
+
+export default App;
